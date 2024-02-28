95b72fade732a4528bbdbefbf3a2040fb93e93f5
Izayah Jordan <izayahjordan13@gmail.com>
2024-02-28 13:42:04 +0000

    changed newReview to html with php inserts

--- newReview.php
+++ newReview.php
@@ -11,29 +11,41 @@ $userGames = $db->prepare('SELECT * FROM `gameDisplay` WHERE `steamID` = \'' . $
 $userGames->execute();
 $userGames = $userGames->fetch();
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+</head>
+<body>
 
-echo '<div class = review>';
-    echo    '<h1>' . $userGames['gameName'] . '<h1>';
-    echo'<div class= "pictureStars">';
-        echo '<img src="'. $userGames["image"] .'">';
-        echo '<div class= reviewFavourite>';
-            echo'<form action="reviewCommit.php" method="post">';
-            echo '<div class="rating">';
-                echo'<input type="text" id="appID" name="appID" value="'. $game .'">';
-                echo'<input id="rating1" type="radio" name="rating" value="1">';
-                echo'<label for="rating1"></label>';
-                echo'<input id="rating2" type="radio" name="rating" value="2">';
-                echo'<label for="rating2"></label>';
-                echo'<input id="rating3" type="radio" name="rating" value="3">';
-                echo'<label for="rating3"></label>';
-                echo'<input id="rating4" type="radio" name="rating" value="4">';
-                echo'<label for="rating4"></label>';
-                echo'<input id="rating5" type="radio" name="rating" value="5">';
-                echo'<label for="rating5"></label>';
-            echo'</div>';
-        echo '</div>';
-        echo '<textarea id="reviewText" name="reviewText" rows="6" cols="50">Add review</textarea>';
-        echo'<input type="submit" value="Post" id="reviewPost">';
-        echo'<form>';
-    echo '</div>';
-echo '</div>';
+<div class = review>
+    <h1><?= $userGames['gameName']; ?><h1>
+    <div class= "pictureStars">
+        <img src="<?= $userGames["image"]; ?>">
+        <div class= reviewFavourite>
+            <form action="reviewCommit.php" method="post">
+            <div class="rating">
+                <input type="text" id="appID" name="appID" value="<?= $game; ?>">
+                <input id="rating1" type="radio" name="rating" value="1">
+                <label for="rating1"></label>
+                <input id="rating2" type="radio" name="rating" value="2">
+                <label for="rating2"></label>
+                <input id="rating3" type="radio" name="rating" value="3">
+                <label for="rating3"></label>
+                <input id="rating4" type="radio" name="rating" value="4">
+                <label for="rating4"></label>
+                <input id="rating5" type="radio" name="rating" value="5">
+                <label for="rating5"></label>
+            </div>
+        </div>
+        <textarea id="reviewText" name="reviewText" rows="6" cols="50">Add review</textarea>
+        <input type="submit" value="Post" id="reviewPost">
+        <form>
+    </div>
+</div>
+
+</body>
