548440cbea7990f1c039e4c0bd6395e91209d743
Izayah Jordan <izayahjordan13@gmail.com>
2024-02-29 15:06:47 +0000

    added deletion to reviews

--- index.php
+++ index.php
@@ -3,9 +3,5 @@ session_start();
 
 require_once  'navbar.php';
 
-echo '<form action="loginForm.php" method="post">';
-echo '<button type="submit" name="logout">Log out</button>';
-echo'</form>';
-
 
 
